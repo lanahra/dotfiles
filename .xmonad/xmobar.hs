a13943f7bca354d199e0c6de1cc6d221a7b93208
Leonardo Nahra <leonardonahra@gmail.com>
2018-02-15 23:49:17 +0100

    Change xmobar colors and format

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,15 +1,19 @@
-Config {
-    font = "xft:Inconsolata:size=12:bold:antialias=true",
-    commands = [ Run Date "%a %b %_d %Y * %H:%M:%S" "theDate" 10,
-                 Run StdinReader,
-                 Run BatteryP [ "BAT0", "BAT1" ]
-                              [ "-t", "<acstatus>: <left>% - <timeleft>",
-                                "--",
-                                "-O", "Charging",
-                                "-i", "Idle",
-                                "-o", "Discharging",
-                                "-h", "green",
-                                "-l", "red" ]
-                                10 ],
-    template = "%StdinReader% }{ %battery% <fc=#FF0000>%theDate%</fc>"
-}
+Config
+    { bgColor = "#002b36"
+    , fgColor = "#839496"
+    , font = "xft:Inconsolata:size=14:antialias=true"
+    , commands =
+        [ Run Date "%d/%m/%y %H:%M" "theDate" 10
+        , Run StdinReader
+        , Run BatteryP
+            [ "BAT0", "BAT1" ]
+            [ "-t", "<acstatus>: <left>% - <timeleft>"
+            , "--"
+            , "-O", "Charging"
+            , "-i", "Idle"
+            , "-o", "Discharging"
+            ]
+            10
+        ]
+    , template = "%StdinReader% }{ %battery% <fc=#fdf6e3>%theDate%</fc>"
+    }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,22 +5,25 @@ import XMonad.Util.Run(spawnPipe)
 import System.IO
 import qualified Data.Map as Map
 
+
+myLogHook xmproc = dynamicLogWithPP xmobarPP
+    { ppOutput = hPutStrLn xmproc
+    , ppTitle = xmobarColor "#859900" "" . shorten 100
+    , ppCurrent = xmobarColor "#268bd2" "" . wrap "[" "]"
+    , ppSep = xmobarColor "#fdf6e3" "" " : "
+    }
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $
+    [ ((modMask, xK_p), spawn "dmenu_run -b") ]
+
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
     xmonad $ defaultConfig
-        {
-            terminal = "urxvt",
-            modMask = mod4Mask,
-            focusedBorderColor = "#ffffff",
-            manageHook = manageDocks <+> manageHook defaultConfig,
-            layoutHook = avoidStruts $ layoutHook defaultConfig,
-            logHook = dynamicLogWithPP xmobarPP
-                {
-                    ppOutput = hPutStrLn xmproc,
-                    ppTitle = xmobarColor "green" "" . shorten 50
-                },
-            keys = \c -> keyMap c `Map.union` keys defaultConfig c
+        { terminal = "urxvt"
+        , modMask = mod4Mask
+        , focusedBorderColor = "#ffffff"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , logHook = myLogHook xmproc
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , keys = \c -> myKeys c `Map.union` keys defaultConfig c
         }
-    where
-        keyMap conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $
-            [ ((modMask, xK_p), spawn "dmenu_run -b") ]
