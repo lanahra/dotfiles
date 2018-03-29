9e6105d395c5a742c98ff902cd353bbbe83d3d3b
Leonardo Nahra <leonardonahra@gmail.com>
2018-03-29 19:44:52 -0300

    Remove brightness and battery for desktop

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -6,27 +6,6 @@ Config
     , commands =
         [ Run Date "%d/%m/%y %H:%M" "date" 10
         , Run StdinReader
-        , Run Brightness
-            [ "-t", "<ipat>"
-            , "-w", "3"
-            , "--"
-            , "-D", "intel_backlight"
-            , "--brightness-icon-patter"
-            , "<icon=brightness/%%.xpm/> <percent>%"
-            ]
-            10
-        , Run Battery
-            [ "-t", "<leftipat>"
-            , "-w", "3"
-            , "--"
-            , "--on-icon-pattern"
-            , "<icon=batteryac/%%.xpm/> <fc=#859900><left>%</fc>"
-            , "--off-icon-pattern"
-            , "<icon=battery/%%.xpm/> <left>%"
-            , "--idle-icon-pattern"
-            , "<icon=batteryac/%%.xpm/> <fc=#268bd2><left>%</fc>"
-            ]
-            10
         , Run DynNetwork
             [ "-t", "<dev> <rxipat> <txipat>"
             , "-S", "true"
@@ -42,7 +21,5 @@ Config
         "%StdinReader% }{ \
         \%dynnetwork% <fc=#fdf6e3>:</fc> \
         \%sound% <fc=#fdf6e3>:</fc> \
-        \%bright% <fc=#fdf6e3>:</fc> \
-        \%battery% <fc=#fdf6e3>:</fc> \
         \<fc=#268bd2>%date%</fc>"
     }
